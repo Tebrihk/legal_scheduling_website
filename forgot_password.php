ee4d108b918d185e8c653774249a27221132df81
Tebrihk Barem KING <baremtebrihk@gmail.com>
2023-12-07 00:07:03 +0100

    edit

---
+++ forgot_password.php
@@ -0,0 +1,260 @@
+<?php
+	ob_start();
+	session_start();
+	
+	$servername = "localhost";
+	$username = "root";
+	$password = "mysql";
+	$dbname = "legal_scheduling";
+	
+	$conn =mysqli_connect($servername,$username,$password,$dbname);
+	$error = false;
+	$emailError = false;
+	$passError = false;
+	
+	if( isset($_POST['login']) ) {	
+		
+		// prevent sql injections/ clear user invalid inputs
+		$email = trim($_POST['email']);
+		$email = strip_tags($email);
+		$email = htmlspecialchars($email);
+		
+		if(empty($email)){
+			$error = true;
+			$emailError = "Please enter your email address.";
+		} else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+			$error = true;
+			$emailError = "Please enter valid email address.";
+		}
+		
+		
+		if (!$error) {
+			
+			$pass = hash('sha256', $password); // password hashing using SHA256
+		
+			$sql=mysqli_query($conn, "SELECT name,email,password FROM client WHERE email='$email'");
+			$row=mysqli_fetch_array($sql);
+			$count = mysqli_num_rows($sql); 
+			if ($count == 1 ) {
+			 $sucMSG = "Paasword sent to email";
+			  $to = $email;
+            $subject = "YOUR NEW PASSWORD WITH MORRIS ODEH CHAMBERS FIRM";
+            $message = "Dear $name, Your password is $pass";
+            $headers = "From: morrisodeh.info@gmail.com";
+
+			 
+			header("refresh:1;url=login.php");
+			} else {
+			
+			  $errMSG = "Your email is not recognised,sign up";
+			  header("refresh:1;url=signup.php");
+			}
+				
+		}
+		
+	}
+		
+		
+	?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>MORRIS ODEH CHAMBERS</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Roboto:wght@300;500;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Contact Start -->
+    <div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="text-center pb-2">
+                <h6 class="text-uppercase" style="font-size:76px; font-family:Verdana, Arial, Helvetica, sans-serif;">
+                    FORGOT PASSWORD</h6>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <div class="contact-form">
+                        <div id="success"></div>
+                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off" method="POST">
+						 <?php
+			if ( isset($errMSG) ) {
+				
+				?>
+				<div class="form-group">
+            	<div class="alert alert-danger">
+				<span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+            	</div>
+                <?php
+			}
+			?>
+			 <?php
+			if ( isset($sucMSG) ) {
+				
+				?>
+				<div class="form-group" style="position: relative;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 1rem;
+  border: 1px solid transparent;
+  border-radius: 5px;
+  font-weight: 700;
+  color:#FFFFFF;
+  background-color:#00FFFF;">
+				<span class="glyphicon glyphicon-info-sign"></span> <?php echo $sucMSG; ?>
+            	</div>
+                <?php
+			}
+			?>
+                            <div class="control-group">
+                                <input type="email" class="form-control p-4" id="email" name="email"
+                                    placeholder="Your email address" required="required"
+                                    data-validation-required-message="Please enter your email address" />
+									<span class="text-danger"><?php echo $emailError; ?></span>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                           
+                    <div>
+                        <button class="btn btn-primary btn-block" type="submit" id="sendMessageButton" name="login">SEND</button>
+						<h3>Don't have an account? <a href="signup.php">sign up now</a></h3>
+                    </div>
+                    </form>
+					
+                </div>
+               
+            </div>
+ <div class="col-lg-6" style="min-height: 400px; float:right;">
+                    <div class="position-relative h-100 rounded overflow-hidden">
+                        <p> <img src="img/feature.jpg" height="400" width="500" border="3px" /></p>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+    <!-- Contact End -->
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-secondary text-white pt-5 px-sm-3 px-md-5" style="margin-top: 90px;">
+        <div class="row mt-5">
+            <div class="col-lg-4">
+                <div class="d-flex justify-content-lg-center p-4" style="background: rgba(256, 256, 256, .05);">
+                    <i class="fa fa-2x fa-map-marker-alt text-primary"></i>
+                    <div class="ml-3">
+                        <h5 class="text-white">Our Office</h5>
+                        <p class="m-0">No 17 Independence way,Kaduna Nigeria.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="d-flex justify-content-lg-center p-4" style="background: rgba(256, 256, 256, .05);">
+                    <i class="fa fa-2x fa-envelope-open text-primary"></i>
+                    <div class="ml-3">
+                        <h5 class="text-white">Email Us</h5>
+                        <p class="m-0">MarkJohnson.info@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="d-flex justify-content-lg-center p-4" style="background: rgba(256, 256, 256, .05);">
+                    <i class="fa fa-2x fa-phone-alt text-primary"></i>
+                    <div class="ml-3">
+                        <h5 class="text-white">Call Us</h5>
+                        <p class="m-0">+234 8193 456 2345</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+        <div class="row pt-5">
+            <div class="col-lg-3 col-md-6 mb-5">
+
+                <div class="d-flex justify-content-start mt-4">
+                    <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i
+                            class="fab fa-twitter"></i></a>
+                    <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i
+                            class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i
+                            class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-lg btn-outline-light btn-lg-square" href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h4 class="font-weight-semi-bold text-primary mb-4">Popular Links</h4>
+                <div class="d-flex flex-column justify-content-start">
+                    <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
+                    <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>About</a>
+                    <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Services</a>
+                    <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Attorney</a>
+                    <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Contact</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+
+                <div class="w-100">
+                    <div class="input-group">
+                        <input type="text" class="form-control border-0" style="padding: 25px;"
+                            placeholder="Your Email">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary px-4">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row p-4 mt-5 mx-0" style="background: rgba(256, 256, 256, .05);">
+            <div class="col-md-6 text-center text-md-left mb-3 mb-md-0">
+                <p class="m-0 text-white">&copy; <a class="font-weight-bold" href="#">Your Site Name</a>. All Rights
+                    Reserved.</p>
+            </div>
+            <div class="col-md-6 text-center text-md-right">
+                <p class="m-0 text-white">Designed by <a class="font-weight-bold"
+                        href="11enterprise.wordpress.com">e11even enterprise</a></p>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary px-3 back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
+<?php ob_end_flush(); ?>
